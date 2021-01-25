167b848f64166a8781686725c5857b07531f95ee
PARK Yong Seo <j@saro.me>
2021-01-25 13:33:18 +0900

    version

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ plugins {
 
 val kitGroupId = "me.saro"
 val kitArtifactId = "kit-ee"
-val kitVersion = "0.1.5.1"
+val kitVersion = "0.1.6"
 
 configure<JavaPluginExtension> {
 	sourceCompatibility = JavaVersion.VERSION_1_8
